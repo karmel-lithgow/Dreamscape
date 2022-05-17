d7520fbe5ec938c0182361024702614ecced100a
Karmel Lithgow <karmel.lithgow@gmail.com>
2022-05-18 11:19:12 +1200

    More changes for heroku deployment

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 const router = require('./routes')
 
+// const config = require('./knexfile').development
 const config = require('./knexfile').development
 const connection = require('knex')(config)
+
+// added for the deployment to heroku
+const environment = process.env.NODE_ENV || 'development'
+
+const db = require('knex')(config)
 
 // this function is to get the dreams list (on homepage)
 function getDreams(db = connection) {
